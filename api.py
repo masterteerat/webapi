97a9140da7479119a3e9e1a1039754ac5c27cb57
masterteerat <teerat.saetan@gmail.com>
2023-01-15 14:57:40 +0700

    add user finder function

--- api.py
+++ api.py
@@ -26,9 +26,11 @@ def api():
             ret = register()
         elif cmd == "order":
             ret = order()
+        elif cmd == "user":
+            ret = user()
         else:
             return jsonify(
-		    result="an error has occured"
+		    result="an error has occured: command not found"
 	        )
 
  
@@ -41,11 +43,17 @@ def register():
     mid = request.form["mid"]
     sid = request.form["sid"]
     name = request.form["name"]
-    
-    sql = "INSERT INTO user (mid, sid, name) VALUES (%s, %s, %s)"
-    val = (mid, sid, name)
-    mycursor.execute(sql, val)
-    mydb.commit()
+    try:
+        sql = "INSERT INTO user (mid, sid, name) VALUES (%s, %s, %s)"
+        val = (mid, sid, name)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        print('register has responded')
+        return jsonify(
+		    result="process failed"
+        )   
     
     print(mid)
     print(sid)
@@ -62,6 +70,21 @@ def order():
     print('menu has responded')
     return jsonify(
 		    result="process complete"
+        
 	)
 
     
+def user():
+
+    sql = "select sid, name from user where mid =%s"
+    mid = (request.form["mid"],)
+
+    mycursor.execute(sql, mid)
+    record = mycursor.fetchone()   
+    print(record)
+    print(record[0])
+    print(record[1])
+
+    return jsonify(
+		result="process complete"
+    )
