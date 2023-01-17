0bb757c0a7ee9a20bbae39ddac5242637cc445d1
masterteerat <teerat.saetan@gmail.com>
2023-01-17 20:56:52 +0700

    user function except error almost complete

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+from mysql.connector.errors import Error
 import mysql.connector
 
 app = Flask(__name__)
@@ -40,14 +41,24 @@ def api():
 
 def register():
 
-    mid = request.form["mid"]
-    sid = request.form["sid"]
-    name = request.form["name"]
     try:
+        mid = request.form["mid"]
+        sid = request.form["sid"]
+        name = request.form["name"]
         sql = "INSERT INTO user (mid, sid, name) VALUES (%s, %s, %s)"
         val = (mid, sid, name)
         mycursor.execute(sql, val)
         mydb.commit()
+    except ValueError as er:
+        print("Function register: value error")
+        return jsonify(
+		    result="process failed"
+        )   
+    except TypeError as er:
+        print("Function register: Type error")
+        return jsonify(
+            result="process failed"
+        )
     except mysql.connector.Error as err:
         print("Something went wrong: {}".format(err))
         print('register has responded')
@@ -76,15 +87,32 @@ def order():
     
 def user():
 
-    sql = "select sid, name from user where mid =%s"
     mid = (request.form["mid"],)
 
-    mycursor.execute(sql, mid)
-    record = mycursor.fetchone()   
-    print(record)
-    print(record[0])
-    print(record[1])
+    try:
+        sql = "select sid, name from user where mid =%s"
+        mycursor.execute(sql, mid)
+        record = mycursor.fetchone()   
+        print(record)
+        print(record[0])
+        print(record[1])
+    except ValueError as er:
+        print("Function user: value error")
+        return jsonify(
+		    result="process failed"
+        )   
+    except TypeError as er:
+        print("Function user: Type error")
+        return jsonify(
+            result="process failed"
+        )
 
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        print('user has responded')
+        return jsonify(
+		    result="process failed"
+        )   
     return jsonify(
-		result="process complete"
-    )
+		    result="process complete"
+        )   
